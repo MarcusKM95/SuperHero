cb4c90fb4fcaa94e485b38f8f8c9662ca25c89bf
MarcusKM95 <mamo0005@stud.kea.dk>
2023-09-18 20:39:59 +0200

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,91 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Superhero s1 = new Superhero("Ken Clark", "Superman", "Strongest creature ever and lasereyes", 1980, 1800);
-        Superhero s2 = new Superhero("Bruce Wayne", "Batman", "Rich, genius-level intellect and utilizes high-tech equipment and weapons", 1939, 800);
-        Superhero s3 = new Superhero("Tony Stark", "Iron man", "powered amor suit", 1963, 1100);
-        Superhero S4 = new Superhero("89P13", "Rocket", "Lazer cannons and blasters", 2014, 600);
-        System.out.println(s1);
+        Scanner scanner = new Scanner(System.in);
+        boolean stopMenu = false;
+        Database db = new Database();
+
+        while(!stopMenu) {
+            System.out.println("velkommen til superheltedatabasen");
+            System.out.println("1: Tilføj en superhelt til databasen");
+            System.out.println("2: Søg efter en Superhelt");
+            System.out.println("3: Vis alle helte i databasen");
+            System.out.println("4: Slet en helt fra databasen");
+            System.out.println("5: Rediger en helt fra databasen");
+            System.out.println("9: for at afslutte programmet");
+
+
+                int valg = scanner.nextInt();
+                switch (valg) {
+                    case 1:
+                        System.out.println("Indtast superheltens navn");
+                        scanner.nextLine();
+                        String navn = scanner.nextLine();
+                        System.out.println("indtast superheltens heltenavn");
+                        String superheltenavn = scanner.nextLine();
+                        System.out.println("Indtast superheltens superkræft");
+                        String superkræft = scanner.nextLine();
+                        System.out.println("Indtast superheltens kreationsår");
+                        int kreationsår = scanner.nextInt();
+                        System.out.println("Indtast superheltens styrke");
+                        int styrke = scanner.nextInt();
+                        Superhero hero = new Superhero(navn, superheltenavn, superkræft, kreationsår, styrke);
+                        db.addSuperHero(hero);
+                        break;
+                    case 2:
+                        System.out.println("Skriv navnet, eller en del af det navn på den/de superhelte du vil finde");
+                        scanner.nextLine();
+                        String søgning = scanner.nextLine();
+                        db.søg(søgning);
+                        break;
+
+
+                    case 3:
+                        db.showHeroList();
+                        break;
+
+                    case 4:
+                        db.showHeroList();
+                        System.out.println("Skriv tallet på den helt du vil slette fra databasen");
+                        scanner.nextLine();
+                        try {
+                            int nummer = scanner.nextInt();
+                            db.slet(nummer);
+                        }
+                        catch (InputMismatchException e) {
+                            System.err.println("Indtast venligst et tal, ikke bogstaver");
+                            scanner.nextLine();
+                        }
+
+
+                        break;
+
+                    case 5:
+                        db.showHeroList();
+                        System.out.println("Skriv tallet på den helt du vil redigere");
+                        scanner.nextLine();
+                        int nummer = scanner.nextInt();
+                        System.out.println("Skriv nu det nye navn til superhelten" + " " + db.superheroList.get(0).getName());
+                        scanner.nextLine();
+                        String naben = scanner.nextLine();
+                        //System.out.println(nummer);
+                        //System.out.println(naben);
+                        db.rediger(nummer, naben);
+                        System.out.println("Det nye navn er nu" + " " + db.superheroList.get(0).getName());
+                        break;
+
+
+                    case 9:
+                        stopMenu = true;
+                        break;
+
+                }
+
+            }
+        }
     }
-}
+
+
--- src/Superhero.java
+++ src/Superhero.java
@@ -38,10 +38,26 @@ public class Superhero {
         return strength;
     }
 
+    //setters
+
+
+    public void setName(java.lang.String name) {
+        this.name = name;
     }
 
+    @Override
+    public String toString() {
+        return "Superhelt:" + "\n" +
+                "Navn:" + " " + name + "\n" +
+                "Superheltenavn:" + " " + superHeroName + "\n" +
+                "Superkræft:" + " " + superPower + "\n" +
+                "skabelsesår:" + " " + creationYear + "\n" +
+                "Styrke:" + " " + strength + "\n";
+    }
+}
+
     /* Skal fremvise en menu (kodes i main, da det er userinterface)
-    hvor det er muligt at oprette en superhelt, søge blandt superhelte, ændre i stats
+    hvor det er muligt at oprette en superhelt, øge blandt superhselte, ændre i stats
     slette superhelte. Der må ikke komme "computersprog" til brugeren, f.eks. true/false
     Programmet må ikke crashe, men skal rette brugeren, f.eks. via try-catch metoder.
     Databasen (arraylist) skal være dynamic sizing, og ikke fixed limit.
